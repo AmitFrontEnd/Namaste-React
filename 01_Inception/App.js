d70252f3b925fe5141f8fb92122137ae5444ef4d
Amit Kumar <amitemailu@gmail.com>
2026-08-05 09:41:13 +0530

    completed ep1

---
+++ 01_Inception/App.js
@@ -0,0 +1,15 @@
+/*
+<div>
+  <div>
+    <h1>heading 1</h1>
+    <h1>heading 2</h1>
+  </div>
+</div>
+*/
+
+const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [
+  React.createElement('h1', {id:'red'}, 'heading 1'),
+  React.createElement('h1', {}, 'heading 2')
+]))
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
