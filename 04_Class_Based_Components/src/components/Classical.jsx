1823ff0810fccc3415c578f3c621c464373582c4
Amit Kumar <amitemailu@gmail.com>
2026-08-13 09:04:43 +0530

    creating states in classical components

--- 04_Class_Based_Components/src/components/Classical.jsx
+++ 04_Class_Based_Components/src/components/Classical.jsx
@@ -3,14 +3,22 @@ import { Component } from "react";
 class Classical extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2:0
+    };
   }
   render() {
     const { name } = this.props;
+    const { count,count2 } = this.state;
     return (
       <div>
         <h1>
           This is classical component <span>({name})</span>
         </h1>
+        <h1>Count is: {count}</h1>
+        <h1>Count is: {count2}</h1>
       </div>
     );
   }
