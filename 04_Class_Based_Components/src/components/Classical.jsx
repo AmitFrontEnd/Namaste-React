c11976fcf39841d8b40aaf3b32e0e7052dfd7d2e
Amit Kumar <amitemailu@gmail.com>
2026-08-13 09:22:39 +0530

    updating state

--- 04_Class_Based_Components/src/components/Classical.jsx
+++ 04_Class_Based_Components/src/components/Classical.jsx
@@ -6,19 +6,33 @@ class Classical extends Component {
 
     this.state = {
       count: 0,
-      count2:0
+      count2: 0,
     };
   }
   render() {
     const { name } = this.props;
-    const { count,count2 } = this.state;
+    const { count, count2 } = this.state;
     return (
       <div>
         <h1>
           This is classical component <span>({name})</span>
         </h1>
         <h1>Count is: {count}</h1>
-        <h1>Count is: {count2}</h1>
+        <button
+          onClick={() => {
+            this.setState({ count: count + 1 });
+          }}
+        >
+          Counter 1 Update button
+        </button>
+        <h1>Count2 is: {count2}</h1>
+        <button
+          onClick={() => {
+            this.setState({ count2: count2 + 1 });
+          }}
+        >
+          Counter 2 Update button
+        </button>
       </div>
     );
   }
